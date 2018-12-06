827b13796f0f95fa8ba4872cdee3d1ea9a69a5df
jbremz <jhbremner@gmail.com>
2018-12-06 13:24:20 +0000

    memory build fix

---
+++ utils.py
@@ -0,0 +1,15 @@
+import sys, os, psutil, gc
+
+def memReport():
+    for obj in gc.get_objects():
+        if torch.is_tensor(obj):
+            print(type(obj), obj.size())
+    
+def cpuStats():
+        print(sys.version)
+        print(psutil.cpu_percent())
+        print(psutil.virtual_memory())  # physical memory usage
+        pid = os.getpid()
+        py = psutil.Process(pid)
+        memoryUse = py.memory_info()[0] / 2. ** 30  # memory use in GB...I think
+        print('memory GB:', memoryUse)
